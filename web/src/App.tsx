82ce0eb3cfc17d04d412cce436a3c921dc2d33d4
Jose Veliz <jose.veliz.francia@gmail.com>
2021-09-18 22:44:08 -0500

    fixes

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,17 +1,14 @@
 import { Router } from "@reach/router";
 
 import { Home } from "./core";
-import { Layout } from "./components";
 
 import "./App.css";
 
 function App() {
   return (
-    <Layout>
-      <Router>
-        <Home path="/" />
-      </Router>
-    </Layout>
+    <Router>
+      <Home path="/" />
+    </Router>
   );
 }
 
