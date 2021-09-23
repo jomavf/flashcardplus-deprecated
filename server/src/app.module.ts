f42379611f27948c0034253c7becb42d9083eb56
Jose Veliz <jose.veliz.francia@gmail.com>
2021-09-22 20:30:33 -0500

    fix mongo url

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -9,12 +9,7 @@ import { Deck1Module } from './core/deck1/deck1.module';
   imports: [
     TypeOrmModule.forRoot({
       type: 'mongodb',
-      // host: 'srv://cluster0.wz85e.mongodb.net',
-      // port: 27017,
-      // username: 'devill96',
-      // password: 'Recordar$01',
-      // database: 'myFirstDatabase',
-      url: 'mongodb+srv://devill96:Recordar$01@cluster0.wz85e.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+      url: 'mongodb+srv://devill96:Recordar$01@cluster0.wz85e.mongodb.net/?retryWrites=true&w=majority',
       entities: [],
       synchronize: true,
     }),
