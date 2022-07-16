65eb30439540d9e534cbf6716680a488680ddd61
Jose Veliz <jose.veliz.francia@gmail.com>
2022-07-16 13:52:29 -0500

    last stuff, not sure what is this

--- web/src/shared/contants.ts
+++ web/src/shared/contants.ts
@@ -1,4 +1,6 @@
-export default {
+export const constants = {
   // API
   API_URL: "http://localhost:5000",
 };
+
+export default constants;
--- web/src/shared/networking.ts
+++ web/src/shared/networking.ts
@@ -1,8 +1,19 @@
-export const networking = {
+const networking = {
   get: async (url: string) => {
-    return await fetch(url, {
+    const response = await fetch(url, {
       method: "GET",
       headers: { "Content-Type": "application/json" },
     });
+    return await response.json();
+  },
+  post: async (url: string, body: any) => {
+    const response = await fetch(url, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(body),
+    });
+    return await response.json();
   },
 };
+
+export default networking;
